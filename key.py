1a6e32f2105bb26a06ee3654927ce269c25b0749
celsius narhwal <hello@celsiusnarhwal.dev>
2024-12-06 22:15:33 -0500

    Update action

---
+++ key.py
@@ -0,0 +1,26 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "github-actions-utils",
+# ]
+# ///
+
+import hashlib
+import importlib.metadata
+import platform
+
+import github_action_utils as gha
+from reflex.utils.prerequisites import get_web_dir
+
+
+web = get_web_dir().absolute()
+key_parts = [platform.system(), platform.machine(), "reflex", importlib.metadata.version("reflex")]
+
+
+for lockfile in ["bun.lockb", "package-lock.json"]:
+    fp = web / lockfile
+
+    if fp.exists():
+        key_parts.append(hashlib.sha256(fp.read_bytes()).hexdigest())
+
+gha.set_output("key", "-".join(key_parts))
