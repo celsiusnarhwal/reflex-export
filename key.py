5ad0b8cca69c4e11010d063c2c37c4e483f97a46
celsius narhwal <hello@celsiusnarhwal.dev>
2024-12-16 10:13:06 -0500

    Update key.py

--- key.py
+++ key.py
@@ -1,15 +1,8 @@
-# /// script
-# requires-python = ">=3.12"
-# dependencies = [
-#     "hachitool",
-# ]
-# ///
-
+import os
 import hashlib
 import importlib.metadata
 import platform
 
-import hachitool
 from reflex.utils.prerequisites import get_web_dir
 
 web = get_web_dir().absolute()
@@ -21,4 +14,4 @@ for lockfile in ["bun.lockb", "package-lock.json"]:
     if fp.exists():
         key_parts.append(hashlib.sha256(fp.read_bytes()).hexdigest())
 
-hachitool.set_output("get_key", "-".join(key_parts))
+print(f"key={'-'.join(key_parts)}", file=open(os.getenv("GITHUB_OUTPUT"), "a"))
